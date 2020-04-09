a6fde5392a4fd6ce261d9f223107758c07e088be
vagrant <j_khabra11@hotmail.com>
2020-04-09 23:42:13 +0000

    tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,20 @@
 const request =  require('request');
-const args = process.argv.slice(2);
-console.log(args);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  }
-  const data = JSON.parse(body);
-  if (data[0]) {
-    console.log(data[0].description);
-  } else {
-    console.log('Breed Not Found');
-  }
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback('Breed Not Found');
+    }
   //console.log('statusCode:', response && response.statusCode);
   //console.log("data:",data);
   //console.log("type of", typeof data);
-});
+  });
+};
+module.exports = { fetchBreedDescription };
